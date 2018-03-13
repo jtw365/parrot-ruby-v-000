e205664e1dddf56e78a92f2a09fec908ac586a8e
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-13 23:58:07 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Squawk")
-  puts "Squawk!"
-  return "Squawk!"
+def parrot(noise="Squawk")
+  puts "#{noise}"
+  return "noise"
 end
